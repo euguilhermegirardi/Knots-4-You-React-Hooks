cf3f57e724852c13fd576acee97d27bda1ad9099
Guilherme Girardi <girardi.gui@icloud.com>
2020-06-15 12:08:56 +0100

    Edit App.js, crete routes.js and global styles file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import { Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { ToastContainer } from 'react-toastify';
+
+import GlobalStyle from './styles/global';
+import Header from './components/Header';
+import Routes from './routes';
+import store from './store/index';
+import history from './services/history';
 
 function App() {
   return (
-    <div className="App">
-      <h1>test</h1>
-    </div>
+    <Provider store={store}>
+      <Router history={history}>
+        <GlobalStyle />
+        <Header />
+        <Routes />
+        <ToastContainer autoClose={3000}/>
+      </Router>
+    </Provider>
   );
 }
 
