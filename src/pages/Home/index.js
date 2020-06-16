0bcc7a671be162529772d33a3370e3c59e5c318f
Guilherme Girardi <girardi.gui@icloud.com>
2020-06-16 12:53:15 +0100

    Create all sections in home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,26 +1,122 @@
 import React from 'react';
+import { IconContext } from "react-icons";
+import { GiBleedingEye } from 'react-icons/gi';
+import { BsFillBagFill } from 'react-icons/bs';
+import { AiOutlineRead } from 'react-icons/ai';
 
-import { Container, ImgContainer, TxtContainer, BtnContainer } from './styles';
+import {
+  Container,
+  ProfileContainer,
+  ImgContainer,
+  TxtContainer,
+  BtnContainer,
+  LearnMacrame,
+  LearnTxtContainer,
+  IconsContainer,
+  GetStartedTxtContainer,
+  Border,
+  GetStarted,
+  SmallTips
+} from './styles';
 import perfil from '../../assets/japa.jpg';
+import beginner from '../../assets/macrame-beginner.jpeg';
+import wall from '../../assets/macrame-wall.jpeg';
+import hanger from '../../assets/macrame-hanger.jpeg';
+import cords from '../../assets/macrame-cords.jpeg';
+
 
 export default function Home() {
   return (
     <>
       <Container>
-        <ImgContainer>
-        <img src={perfil} alt="Perfil"/>
-        </ImgContainer>
+        <ProfileContainer>
+          <ImgContainer>
+            <img src={perfil} alt="Perfil" />
+          </ImgContainer>
 
-        <TxtContainer>
-          <h2>Hi, I'm Raisa</h2>
-          <p>
-            Through a range of online tutorials and learning resources I teach crafty folk how to make quality macrame as well as advise inspired creatives on how to transform their handmade hobby, into a profitable side hustle or business.
+          <TxtContainer>
+            <h2>Hi, I'm Raisa</h2>
+            <p>
+              Through a range of online tutorials and learning resources I teach crafty folk how to make quality macrame as well as advise inspired creatives on how to transform their handmade hobby, into a profitable side hustle or business.
           </p>
-          <BtnContainer>
-            <button type="button">Learn Macrame</button>
-            <button type="button">Uplevel Your Hobby</button>
-          </BtnContainer>
-        </TxtContainer>
+            <BtnContainer>
+              <button type="button">Macrame Tips</button>
+              <button type="button">Uplevel Your Hobby</button>
+            </BtnContainer>
+          </TxtContainer>
+        </ProfileContainer>
+
+        <LearnMacrame>
+          <LearnTxtContainer>
+            <h2>Do you want to learn how to master macramé patterns?</h2>
+            <h3>Or are you simply looking for a place to source the best quality cotton cord?</h3>
+            <h3>Either way, United Knots is here to teach you how to macramé your way to making and designing your own beautiful pieces.</h3>
+          </LearnTxtContainer>
+          <IconContext.Provider value={{ color: "#474547", size: "5.5rem" }}>
+            <IconsContainer>
+              <div>
+                <button type="button">Watch Video Tutorials</button>
+                <GiBleedingEye />
+              </div>
+              <div>
+                <button type="button">Shop Supplies</button>
+                <BsFillBagFill />
+              </div>
+              <div>
+                <button type="button">Read The Blog</button>
+                <AiOutlineRead />
+              </div>
+            </IconsContainer>
+          </IconContext.Provider>
+        </LearnMacrame>
+
+        <GetStartedTxtContainer>
+          <h2>Let’s get started...</h2>
+          <h3>Small tips for you!</h3>
+        </GetStartedTxtContainer>
+        <Border />
+        <GetStarted>
+          <SmallTips>
+            <h3>Beginners Macrame Knots - 6. Double Half Hitch Knots</h3>
+            <img src={beginner} alt="Beginner"/>
+            <p>
+              If you've chosen to take the first steps to learning a new craft, fantastic!
+              Welcome aboard, here is what you need to know in Basic steps Knot 6- How To Tie Double Half Hitch Knots
+            </p>
+            <button type="button">Read More</button>
+          </SmallTips>
+
+          <SmallTips>
+            <h3>Beginners Macrame Knots - 6. Double Half Hitch Knots</h3>
+            <img src={wall} alt="Beginner"/>
+            <p>
+              If you've chosen to take the first steps to learning a new craft, fantastic!
+              Welcome aboard, here is what you need to know in Basic steps Knot 6- How To Tie Double Half Hitch Knots
+            </p>
+            <button type="button">Read More</button>
+          </SmallTips>
+
+          <SmallTips>
+            <h3>Beginners Macrame Knots - 6. Double Half Hitch Knots</h3>
+            <img src={hanger} alt="Beginner"/>
+            <p>
+              If you've chosen to take the first steps to learning a new craft, fantastic!
+              Welcome aboard, here is what you need to know in Basic steps Knot 6- How To Tie Double Half Hitch Knots
+            </p>
+            <button type="button">Read More</button>
+          </SmallTips>
+
+          <SmallTips>
+            <h3>Beginners Macrame Knots - 6. Double Half Hitch Knots</h3>
+            <img src={cords} alt="Beginner"/>
+            <p>
+              If you've chosen to take the first steps to learning a new craft, fantastic!
+              Welcome aboard, here is what you need to know in Basic steps Knot 6- How To Tie Double Half Hitch Knots
+            </p>
+            <button type="button">Read More</button>
+          </SmallTips>
+
+        </GetStarted>
       </Container>
     </>
   )
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -2,9 +2,14 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   display: flex;
+  flex-direction: column;
   max-width: 1400px;
   max-height: 550px;
   margin: 0 auto;
+`;
+
+export const ProfileContainer = styled.section`
+  display: flex;
 `;
 
 export const ImgContainer = styled.div`
@@ -61,3 +66,148 @@ export const BtnContainer = styled.div`
   }
 `;
 
+export const LearnMacrame = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  min-height: 420px;
+  margin: 6rem;
+`;
+
+export const LearnTxtContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin: 3rem 0 2rem 0;
+
+  h2 {
+    font-size: 2rem;
+  }
+
+  h3 {
+    font-size: 1.5rem;
+    font-weight: 100;
+
+    &:nth-child(2) {
+      margin: 1.5rem 0;
+    }
+  }
+`;
+
+export const IconsContainer = styled.div`
+  display: flex;
+
+  div {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    &:nth-child(2) {
+      margin: 0 10rem;
+    }
+
+    button {
+    font-size: 1rem;
+    background: #f8bbd0;
+    letter-spacing: 0.02em;
+    text-align: center;
+    border: 0;
+    color: inherit;
+    padding: 1rem;
+    transition: ease .5s;
+    margin-bottom: 1.2rem;
+
+    &:nth-child(2) {
+      margin-left: 2rem;
+    }
+
+    &:hover {
+      background: #c9b2ba;
+      color: #fff;
+    }
+  }
+  }
+`;
+
+export const GetStartedTxtContainer = styled.div`
+  background-color: #fce4ec;
+  text-align: center;
+  padding: 1rem 1rem 0 1rem;
+
+  h2 {
+    font-size: 2.2rem;
+    letter-spacing: 2px;
+  }
+
+  h3 {
+    font-size: 1.6rem;
+    letter-spacing: 1px;
+    font-weight: normal;
+    margin: 5px 0 8px 0;
+  }
+`;
+
+export const Border = styled.section`
+  border: 2px solid #ccc;
+`;
+
+export const GetStarted = styled.section`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(400px, 1fr) );
+  justify-items: center;
+  background-color: #fce4ec;
+  padding: 1rem 3rem 3rem 3rem;
+`;
+
+export const SmallTips = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  max-width: 380px;
+  padding: 1rem;
+
+  h3 {
+    text-align: center;
+    margin-bottom: 8px;
+    font-size: 1.3rem;
+    cursor: pointer;
+  }
+
+  img {
+    height: 300px;
+    width: 210px;
+    cursor: pointer;
+  }
+
+  p {
+    cursor: pointer;
+    font-size: 1rem;
+    margin-top: 8px;
+    line-height: 1.3rem;
+    letter-spacing: 1px;
+  }
+
+  button {
+    font-size: 1rem;
+    background: #c9b2ba;
+    letter-spacing: 0.09em;
+    text-align: center;
+    border: 0;
+    color: #fff;
+    padding: 1rem 2.3rem;
+    transition: ease .5s;
+    margin: 1.2rem 0;
+
+    &:nth-child(2) {
+      margin-left: 2rem;
+    }
+
+    &:hover {
+      background: #f8bbd0;
+      color: inherit;
+    }
+  }
+`;
