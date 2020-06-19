6f9b2b9dcb8799ed3a1cd31dbb046ead22b185be
Guilherme Girardi <girardi.gui@icloud.com>
2020-06-19 17:20:41 +0100

    Create About page

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,7 +1,53 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import {
+  Container,
+  MacrameHistory
+} from './styles';
+import macrame from '../../assets/History/macrame-history.jpg';
+import macrame2 from '../../assets/History/macrame-history2.jpg';
+import api from '../../services/api';
+import Footer from '../../components/Footer';
 
 export default function About() {
+  const [text, setText] = useState([]);
+
+  useEffect(() => {
+    async function loadData() {
+      api.get('history')
+        .then(response => {
+          setText(response.data)
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    };
+
+    loadData();
+  },[]);
+
   return (
-    <h1>About</h1>
+    <>
+      <Container>
+        <MacrameHistory>
+
+          {text.map(txt => (
+            <li key={txt.key}>
+              <h2>{txt.header}</h2>
+              <p>{txt.firsttext}</p>
+              <p>{txt.secondtext}</p>
+              <img src={macrame} alt="Macrame.jpg"/>
+              <p>{txt.thirdtext}</p>
+              <img src={macrame2} alt="Macrame.jpg"/>
+              <p>{txt.fourthtext}</p>
+              <small>Reference: {txt.ref}</small>
+              <small>Reference:  {txt.reftwo}</small>
+            </li>
+          ))}
+
+        </MacrameHistory>
+      </Container>
+      <Footer />
+    </>
   )
 };
---
+++ src/pages/About/styles.js
@@ -0,0 +1,46 @@
+import styled from 'styled-components';
+
+export const Container = styled.section`
+  display: flex;
+  flex-direction: column;
+  max-width: 1020px;
+  margin: 0 auto;
+  padding: 1rem;
+`;
+
+export const MacrameHistory = styled.div`
+  background-color: #fce4ec;
+  color: inherit;
+  padding: 2rem;
+
+  li {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    h2 {
+      font-size: 2.2rem;
+      margin-bottom: 1rem;
+    }
+
+    p {
+      font-size: 1.1rem;
+      letter-spacing: 2px;
+      line-height: 1.5rem;
+    }
+
+    p:nth-child(2) {
+      margin-bottom: 2.2rem;
+    }
+
+    img {
+      margin: 2rem;
+      border-radius: 2px;
+    }
+
+    small {
+      margin-top: 1.8rem;
+    }
+  }
+`;
