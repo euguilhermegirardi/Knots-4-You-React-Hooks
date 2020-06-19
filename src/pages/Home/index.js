63d6d26c2b7b8351014f2602c7a9004e51765f9e
Guilherme Girardi <girardi.gui@icloud.com>
2020-06-19 16:05:57 +0100

    Create new object at server.json and edit home pag

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { IconContext } from "react-icons";
 import { GiBleedingEye } from 'react-icons/gi';
 import { BsFillBagFill } from 'react-icons/bs';
@@ -18,7 +18,8 @@ import {
   GetStarted,
   SmallTips
 } from './styles';
-import perfil from '../../assets/japa.jpg';
+import api from '../../services/api';
+import profile from '../../assets/japa.jpg';
 import beginner from '../../assets/macrame-beginner.jpeg';
 import wall from '../../assets/macrame-wall.jpeg';
 import hanger from '../../assets/macrame-hanger.jpeg';
@@ -26,98 +27,107 @@ import cords from '../../assets/macrame-cords.jpeg';
 import Footer from '../../components/Footer';
 
 export default function Home() {
+  const [text, setText] = useState([]);
+
+  useEffect(() => {
+    async function getText() {
+      api.get('homepage')
+        .then(response => {
+          setText(response.data)
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+
+    getText();
+  }, []);
+
+  console.log(text);
+
   return (
     <>
       <Container>
-        <ProfileContainer>
-          <ImgContainer>
-            <img src={perfil} alt="Perfil" />
-          </ImgContainer>
+        {text.map(txt => (
+          <li key={txt.id}>
+            <ProfileContainer>
 
-          <TxtContainer>
-            <h2>Hi, I'm Raisa</h2>
-            <p>
-              Through a range of online tutorials and learning resources I teach crafty folk how to make quality macrame as well as advise inspired creatives on how to transform their handmade hobby, into a profitable side hustle or business.
-          </p>
-            <BtnContainer>
-              <button type="button">Macrame Tips</button>
-              <button type="button">Uplevel Your Hobby</button>
-            </BtnContainer>
-          </TxtContainer>
-        </ProfileContainer>
+              <ImgContainer>
+                <img src={profile} alt="Profile" />
+              </ImgContainer>
 
-        <LearnMacrame>
-          <LearnTxtContainer>
-            <h2>Do you want to learn how to master macramé patterns?</h2>
-            <h3>Or are you simply looking for a place to source the best quality cotton cord?</h3>
-            <h3>Either way, United Knots is here to teach you how to macramé your way to making and designing your own beautiful pieces.</h3>
-          </LearnTxtContainer>
-          <IconContext.Provider value={{ color: "#474547", size: "5.5rem" }}>
-            <IconsContainer>
-              <div>
-                <button type="button">Watch Video Tutorials</button>
-                <GiBleedingEye />
-              </div>
-              <div>
-                <button type="button">Shop Supplies</button>
-                <BsFillBagFill />
-              </div>
-              <div>
-                <button type="button">Read The Blog</button>
-                <AiOutlineRead />
-              </div>
-            </IconsContainer>
-          </IconContext.Provider>
-        </LearnMacrame>
+              <TxtContainer>
+                <h2>Hi, I'm {txt.name}</h2>
+                <p>{txt.profile}</p>
+                <BtnContainer>
+                  <button type="button">Macrame Tips</button>
+                  <button type="button">Uplevel Your Hobby</button>
+                </BtnContainer>
+              </TxtContainer>
+            </ProfileContainer>
 
-        <GetStartedTxtContainer>
-          <h2>Let’s get started...</h2>
-          <h3>Small tips for you!</h3>
-        </GetStartedTxtContainer>
-        <Border />
-        <GetStarted>
-          <SmallTips>
-            <h3>Beginners Macrame Knots - 6. Double Half Hitch Knots</h3>
-            <img src={beginner} alt="Beginner"/>
-            <p>
-              If you've chosen to take the first steps to learning a new craft, fantastic!
-              Welcome aboard, here is what you need to know in Basic steps Knot 6- How To Tie Double Half Hitch Knots
-            </p>
-            <button type="button">Read More</button>
-          </SmallTips>
+            <LearnMacrame>
+              <LearnTxtContainer>
+                <h2>{txt.question}</h2>
+                <h3>{txt.question2}</h3>
+                <h3>{txt.paragraph}</h3>
+              </LearnTxtContainer>
+              <IconContext.Provider value={{ color: "#474547", size: "5.5rem" }}>
+                <IconsContainer>
+                  <div>
+                    <button type="button">Watch Video Tutorials</button>
+                    <GiBleedingEye />
+                  </div>
+                  <div>
+                    <button type="button">Shop Supplies</button>
+                    <BsFillBagFill />
+                  </div>
+                  <div>
+                    <button type="button">Read The Blog</button>
+                    <AiOutlineRead />
+                  </div>
+                </IconsContainer>
+              </IconContext.Provider>
+            </LearnMacrame>
 
-          <SmallTips>
-            <h3>Beginners Macrame Knots - 6. Double Half Hitch Knots</h3>
-            <img src={wall} alt="Beginner"/>
-            <p>
-              If you've chosen to take the first steps to learning a new craft, fantastic!
-              Welcome aboard, here is what you need to know in Basic steps Knot 6- How To Tie Double Half Hitch Knots
-            </p>
-            <button type="button">Read More</button>
-          </SmallTips>
+            <GetStartedTxtContainer>
+              <h2>{txt.getstarted}</h2>
+              <h3>{txt.smalltips}</h3>
+            </GetStartedTxtContainer>
+            <Border />
+            <GetStarted>
+              <SmallTips>
+                <h3>{txt.tiptopic}</h3>
+                <img src={beginner} alt="Beginner" />
+                <p>{txt.tiptext}</p>
+                <button type="button">Read More</button>
+              </SmallTips>
 
-          <SmallTips>
-            <h3>Beginners Macrame Knots - 6. Double Half Hitch Knots</h3>
-            <img src={hanger} alt="Beginner"/>
-            <p>
-              If you've chosen to take the first steps to learning a new craft, fantastic!
-              Welcome aboard, here is what you need to know in Basic steps Knot 6- How To Tie Double Half Hitch Knots
-            </p>
-            <button type="button">Read More</button>
-          </SmallTips>
+              <SmallTips>
+                <h3>{txt.tiptopic}</h3>
+                <img src={wall} alt="Beginner" />
+                <p>{txt.tiptext}</p>
+                <button type="button">Read More</button>
+              </SmallTips>
 
-          <SmallTips>
-            <h3>Beginners Macrame Knots - 6. Double Half Hitch Knots</h3>
-            <img src={cords} alt="Beginner"/>
-            <p>
-              If you've chosen to take the first steps to learning a new craft, fantastic!
-              Welcome aboard, here is what you need to know in Basic steps Knot 6- How To Tie Double Half Hitch Knots
-            </p>
-            <button type="button">Read More</button>
-          </SmallTips>
+              <SmallTips>
+                <h3>{txt.tiptopic}</h3>
+                <img src={hanger} alt="Beginner" />
+                <p>{txt.tiptext}</p>
+                <button type="button">Read More</button>
+              </SmallTips>
 
-        </GetStarted>
+              <SmallTips>
+                <h3>{txt.tiptopic}</h3>
+                <img src={cords} alt="Beginner" />
+                <p>{txt.tiptext}</p>
+                <button type="button">Read More</button>
+              </SmallTips>
 
+            </GetStarted>
+
+          </li>
+        ))}
       </Container>
       <Footer />
     </>
